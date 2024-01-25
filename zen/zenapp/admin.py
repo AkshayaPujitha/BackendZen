9da36e57b54c260d4f00737e3f974d7ba80f4c10
pujitha <akshayapujithakolli@gmail.com>
2024-01-25 23:53:52 +0530

    models for user problem and image of array added

--- zen/zenapp/admin.py
+++ zen/zenapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import UserModel,UserProblem,UserProblemImage
 # Register your models here.
 
 admin.site.register(UserModel)
+admin.site.register(UserProblemImage)
+admin.site.register(UserProblem)
+
--- zen/zenapp/models.py
+++ zen/zenapp/models.py
@@ -5,3 +5,12 @@ class UserModel(models.Model):
     def __str__(self):
         return f"{self.user}"
     
+class UserProblem(models.Model):
+    user = models.ForeignKey(UserModel, related_name='images', on_delete=models.CASCADE)
+    description = models.TextField()
+    images = models.ManyToManyField('UserProblemImage', related_name='user_problems', blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+class UserProblemImage(models.Model):
+    image = models.ImageField(upload_to='user_problem_images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
